69aa0db6415e4b16bce9251893a655940c7d8b99
leji-a <toviya3135@gmail.com>
2024-04-19 13:49:35 -0300

    Funciones validarEntradas, calcularMontoFinal, mostrarResultado fueron agregadas

--- script/script.js
+++ script/script.js
@@ -1,9 +1,31 @@
 function calcularPlazoFijo() {
+    const monto = parseFloat(document.getElementById('monto').value);
+    const dias = parseInt(document.getElementById('dias').value);
+    const reinvertir = document.getElementById('reinvertir').checked;
+
+    if (!validarEntradas()) return;
+
+    let montoFinal = calcularMontoFinal(monto, dias);
+
+    mostrarResultado(monto, dias, reinvertir);
+}
+function calcularPorcentaje(dias){
+    if (dias >= 30 && dias <= 60){ 
+        return 0.4
+    } else if (dias >= 61 && dias <= 120){ 
+        return 0.45
+    } else if(dias >= 121 && dias <= 360){ 
+        return 0.5
+    } else if(dias > 360) {
+        return 0.65 
+    }
+    return 0;
+}
+function validarEntradas() {
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
     const monto = parseFloat(document.getElementById('monto').value);
     const dias = parseInt(document.getElementById('dias').value);
-    const reinvertir = document.getElementById('reinvertir').checked;
 
     document.getElementById('errorNombre').textContent = '';
     document.getElementById('errorNombreNumero').textContent = '';
@@ -28,35 +50,29 @@ function calcularPlazoFijo() {
         document.getElementById('errorDias').textContent = "La cantidad de días debe ser mayor o igual a 30.";
         error = true;
     }
-    if (error) return;
+
+    return !error;
+}
+function calcularMontoFinal(monto, dias) {
+    let porcentaje = calcularPorcentaje(dias);
+    return monto + (monto * (dias / 360) * porcentaje);
+}
+function mostrarResultado(monto, dias, reinvertir) {
     
-    let porcentaje = calcularPorcentaje(dias)
-    let montoFinal = monto + (monto * (dias / 360) * porcentaje);
+    document.getElementById('resultado').textContent = ''
     
     if (!reinvertir) {
+        let montoFinal = calcularMontoFinal(monto, dias);
         document.getElementById('resultado').textContent = `El monto final a recibir es: ${montoFinal.toFixed(2)}`; 
     } else {
         let tabla = "<table><tr><th>Período</th><th>Monto Inicial</th><th>Monto Final</th></tr>";
         let montoInicial = monto;
         for (let i = 1; i <= 4; i++) {
-            montoFinal = montoInicial + (montoInicial * (dias / 360) * porcentaje);
+            let montoFinal = calcularMontoFinal(montoInicial, dias);
             tabla += `<tr><td>${i}</td><td>${montoInicial.toFixed(4)}</td><td>${montoFinal.toFixed(4)}</td></tr>`;
             montoInicial = montoFinal;
         }
         tabla += "</table>";
         document.getElementById('resultado').innerHTML = tabla;
-        }
-
-}
-function calcularPorcentaje(dias){
-    if (dias >= 30 && dias <= 60){ 
-        return 0.4
-    } else if (dias >= 61 && dias <= 120){ 
-        return 0.45
-    } else if(dias >= 121 && dias <= 360){ 
-        return 0.5
-    } else if(dias > 360) {
-        return 0.65 
     }
-    return 0;
 }
